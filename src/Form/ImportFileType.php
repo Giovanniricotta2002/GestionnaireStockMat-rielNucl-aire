beb47d4aed345c225607365d2a6caf9c1ccc6479
Gio <giovanni.ricotta@ynov.com>
2025-01-17 16:09:10 +0100

    main: Added csrf token form

--- src/Form/ImportFileType.php
+++ src/Form/ImportFileType.php
@@ -32,6 +32,12 @@ class ImportFileType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => ImportFile::class,
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'inport_file',
         ]);
     }
 }
--- src/Form/MaterielInspectionRechercheType.php
+++ src/Form/MaterielInspectionRechercheType.php
@@ -56,6 +56,12 @@ class MaterielInspectionRechercheType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => MaterielInspectionRecherche::class,
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'recherche_type',
         ]);
     }
 }
